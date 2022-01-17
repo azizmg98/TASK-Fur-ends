36d39b6eaa06c669f01d5cf2dfe24770510d76e1
aziz <azizalghanim98@gmail.com>
2022-01-17 21:38:09 +0300

    section 1 done, section 2 attempted

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 export default function PetItem(props) {
   const pet = props.pet;
+  const [petPic, setPet] = useState(pet.image)
+  const petPet = () =>{
+    setPet(pet.image2)
+  }
+
   return (
-    <div class="col-lg-4 col-md-8 col-sm-10">
-      <div class="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
-        <div class="content">
+    <div className="col-lg-4 col-md-8 col-sm-10">
+      <div className="single-doctor">
+        <img className="image" alt={pet.name} src={petPic} />
+        <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" class="btn btn-info">
+          <button type="button" className="btn btn-info" onClick={petPet}>
             Pet
           </button>
         </div>
