b026a8231fcecd1ead58f38202834b43a1519012
aziz <azizalghanim98@gmail.com>
2022-03-02 22:39:21 +0300

    pet adopt complete

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 export default function PetItem(props) {
   const pet = props.pet;
-  const [petPic, setPet] = useState(pet.image)
-  const petPet = () =>{
-    setPet(pet.image2)
-  }
+  const [petPic, setPet] = useState(pet.image);
+  const petPet = () => {
+    setPet(pet.image2);
+  };
 
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
@@ -15,6 +15,18 @@ export default function PetItem(props) {
           <h3>{pet.name}</h3>
           <button type="button" className="btn btn-info" onClick={petPet}>
             Pet
+          </button>
+          <br />
+          <button
+            type="button"
+            className="btn btn-info mt-2"
+            onClick={() => {
+              if (window.confirm(`do you want to adopt ${pet.name}`)) {
+                props.handleAdopt(pet.id);
+              }
+            }}
+          >
+            Adopt
           </button>
         </div>
       </div>
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,16 +1,22 @@
 import PetItem from "./PetItem";
 import { useState } from "react";
-import SearchBar from './SearchBar'
-import Selector from './Selector'
+import SearchBar from "./SearchBar";
+import Selector from "./Selector";
 
 export default function PetsList(props) {
   const [query, setQuery] = useState("");
   const [type, setType] = useState("");
+  const [petsCopy, setPetsCopy] = useState(props.pets);
+  const handleAdopt = (petId) =>
+    setPetsCopy(petsCopy.filter((pet) => pet.id !== petId));
+  // naming should be what the thing does not what it is?
 
-  const pets = props.pets
+  // can we destructor props and use it as is?
+  // yes but naming issues + it's more explicit
+  const pets = petsCopy
     .filter((pet) => pet.name.toLowerCase().includes(query))
     .filter((pet) => pet.type.includes(type))
-    .map((pet) => <PetItem key={pet.id} pet={pet} />);
+    .map((pet) => <PetItem key={pet.id} pet={pet} handleAdopt={handleAdopt} />);
 
   return (
     <section id="doctors" className="doctor-section pt-140">
@@ -21,7 +27,7 @@ export default function PetsList(props) {
               <h1 className="mb-25 wow fadeInUp" data-wow-delay=".2s">
                 Fur-ends
               </h1>
-              <SearchBar setQuery={setQuery}/>
+              <SearchBar setQuery={setQuery} />
               <br />
               Type:
               <Selector setType={setType} />
